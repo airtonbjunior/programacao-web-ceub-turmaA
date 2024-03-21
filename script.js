9600bd156ae9030311219053c91b5482a6ee0323
airtonbjunior <airtonbjunior@gmail.com>
2024-03-21 09:41:32 -0300

    atualizacoes aula javascript

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
-let var1 = "30";
-let var2 = 5;
+let var1 = 0.1;
+let var2 = 0.1;
 
-let resultado1 = var1 - var2;
 let resultado2 = var1 + var2;
 
-console.log(resultado1);
-console.log(typeof(resultado1));
+//console.log(resultado2);
+//console.log(typeof(resultado2));
 
-console.log(resultado2);
-console.log(typeof(resultado2));
+let variavelArray = [5, 4, 8, 0, -7, "abc", 8.3];
+console.log(variavelArray[5]);
 
+let variavelObjeto = {
+    nome: "Airton",
+    idade: "18",
+    altura: 1.90,
+    cpf: "65654654654",
+    endereco: {
+        rua: "abc",
+        bairro: "xyz",
+        numero: 1,
+        apartamento: {
+            aaaa: "ddd",
+            bbbb: "hdhdhd"
+        }
+    }
+}
+
+
+
+if(variavelObjeto.idade === 18) {
+    console.log("Pode entrar no meu site!");
+} else {
+    console.log("Não pode entrar no meu site");
+}
+
+// != diferente
+// == igual
+// > maior
+// < menor
+// >= maior ou igual
+// <= menor ou igual
+
+//console.log(variavelObjeto.endereco.apartamento.aaaa);
+
+//console.log(variavelObjeto["nome"]);
+//console.log(variavelObjeto.nome);
 
 
 
