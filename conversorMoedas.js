e87ddb021b74c0ed83046f5173c4a39e1f87372c
airtonbjunior <airtonbjunior@gmail.com>
2024-04-11 10:45:06 -0300

    atula 11abril

--- conversorMoedas.js
+++ conversorMoedas.js
@@ -13,12 +13,41 @@ let valoresConversao = {
     }
 }
 
+let valorUsuario = document.getElementById("valor-usuario");
+valorUsuario.addEventListener("keypress", function(event) {
+
+    console.log(event);
+
+    if(event.ctrlKey == true && event.code == "KeyI") {
+        inverter();
+    }
+
+    if(event.key == "Enter") {
+        converter();
+    }
+
+});
+
+
+
+function limpar() {
+    let valorUsuario = document.getElementById("valor-usuario");
+    let resultado = document.getElementById("resultado");
+
+    valorUsuario.value = "";
+    resultado.textContent = "";
+}
 
 function converter() {
     let valorUsuario = document.getElementById("valor-usuario").value;
 
     let moedaOrigem  = document.getElementById("moeda1").value;
     let moedaDestino = document.getElementById("moeda2").value;
+
+    if(valorUsuario == "") {
+        alert("Valor não pode ser vazio!");
+        return;
+    }
 
     if(moedaOrigem == moedaDestino) {
         alert("As moedas são iguais, não é possível converter");
