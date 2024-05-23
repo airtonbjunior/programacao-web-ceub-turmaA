382951298c1c3d4d44cc32ee1d175d73c684ae6e
airtonbjunior <airtonbjunior@gmail.com>
2024-05-23 10:45:24 -0300

    backend atualizado

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,47 @@ aplicacao.post('/', (req, res) => {
     res.send("Meu backend funcionando com método POST");
 });
 
+aplicacao.get('/moedas', (req, res) => {
+    const moedas = {
+        BRL: "real",
+        USD: "dolar",
+        EUR: "euro"
+    }
+
+    res.status(200).json(moedas);
+})
+
+aplicacao.post('/moedas', (req, res) => {
+    const moedas = {
+        BRL: "real",
+        USD: "dolar",
+        EUR: "euro"
+    }
+
+    res.status(200).json(moedas);    
+})
+
+aplicacao.get('/conversao/:moedas', (req, res) => {
+    //console.log(req.params.moedas);
+    let moedas = req.params.moedas.split("-");
+
+    let moeda1 = moedas[0];
+    let moeda2 = moedas[1];
+
+    console.log(moeda1);
+    console.log(moeda2);
+
+    const resultado = {
+        moedaOrigem: moeda1,
+        moedaDestino: moeda2
+        // fatorDeConversao: algumacoisas
+    };
+
+    // Retornar o fator de conversão entre moeda1 e moeda2 passada como parametro da url
+    res.status(200).json(resultado);
+});
+
+
 aplicacao.listen(4000, () => {
     console.log("Estou escutando na porta 4000");
 })
